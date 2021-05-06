8ccfba812030742e65592ed06c5885ac724830ed
Tao Hong <76576903+Junyingli510@users.noreply.github.com>
2021-05-06 21:51:42 +0300

    Update python_selenium_options.py

--- selenium/python_selenium_options.py
+++ selenium/python_selenium_options.py
@@ -10,7 +10,7 @@ CHROME_OPTIONS.add_argument("--headless")
 CHROME_OPTIONS.add_argument('--disable-gpu')  # Last I checked this was necessary.
 CHROME_OPTIONS.add_argument('--disable-notifications')
 prefs = {"profile.managed_default_content_settings.images": 2, 'disk-cache-size': 4096}
-CHROME_OPTIONS.add_experimental_option('prefs', prefs)
+CHROME_OPTIONS.add_experimental_option('prefs', prefs) # image loading disable
 CHROME_OPTIONS.add_argument('--ignore-certificate-errors')
 CHROME_OPTIONS.add_argument("--test-type")
 CHROME_OPTIONS.add_argument('--disable-infobars')
