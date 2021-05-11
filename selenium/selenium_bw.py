5c0b271421cbac0862d0a2bec54f0e39958c68d3
Tao Hong <76576903+Junyingli510@users.noreply.github.com>
2021-05-11 07:44:42 +0300

    Update selenium_bw.py

--- selenium/selenium_bw.py
+++ selenium/selenium_bw.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 
 # time lib
